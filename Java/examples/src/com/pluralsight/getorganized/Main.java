95921adb0b49a4d1a64c5c9037e1f5e0f2005ec9
kevindabhi <36810148+kevindabhi@users.noreply.github.com>
2018-03-07 00:13:37 +0530

    Add files via upload
    On day 8 and 9 I did Java after finding a course on pluralsight which was sponsored by Google for learning android development. 
    Also some HTML pages from MDN.

---
+++ Java/examples/src/com/pluralsight/getorganized/Main.java
@@ -0,0 +1,75 @@
+package com.pluralsight.getorganized;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        //Operator precedence example
+
+        /*
+        int valA = 21;
+	int valB = 6;
+	int valC = 3;
+	int valD = 1;
+
+	int result1 = valA - valB / valC;
+	int result2 = (valA - valB) / valC;
+    int result3 = valA / valC * valD + valB;
+    int result4 = valA / (valC * (valD + valB));
+	System.out.println(result1);
+	System.out.println(result2);
+	System.out.println(result3);
+	System.out.println(result4);
+	*/
+
+
+        // type conversion
+
+/*float floatVal = 1.0f;
+double doubleVal = 4.0d;
+byte byteVal = 7;
+short shortVal = 7;
+long longVal = 5;
+
+short result1 = (short)byteVal;
+short result2 = (short) (byteVal - longVal);
+double result3 = longVal - doubleVal;
+long result4 = (long) (shortVal - longVal + floatVal + doubleVal);
+
+System.out.println ("Success"); */
+
+
+
+//Nested If Statements
+/*
+float students =0.0f;
+float rooms = 4.0f;
+
+    if (students > 0.0f) {
+        if (rooms > 0.0f)
+            System.out.println(students / rooms);
+    }
+        else
+        System.out.println("No students");
+
+
+
+System.out.println();
+System.out.println("** End program **");
+*/
+
+// Logical operators example
+/*
+        int students =150, rooms = 0;
+
+            if(rooms> 0 && students/rooms > 30)
+                System.out.println("crowded!");
+
+        System.out.println();
+        System.out.println("** End Program**");
+*/
+
+
+    }
+}
---
+++ Java/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+/* ths iis the first line of the coment.
+
+ */
+    public static void main(String[] args) {
+        //This is a standalone comment
+	System.out.println("Hello world Again!"); //This is a comment
+       // System.out.println("Hello world Again Two");
+        System.out.println(/*"Hello world Again Three"*/ "this is a different text");
+    }
+}
